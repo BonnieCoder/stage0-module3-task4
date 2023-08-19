9735a2ee09a986fc7da81540711187194ec6335f
Muslima Akbarova <68753473+BonnieCoder@users.noreply.github.com>
2023-08-19 23:48:17 +0500

    Update FormulaCreator.java

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -1,11 +1,13 @@
 package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
+    static double formula(int a, int b){
+        return (9 * a * a - 5 * b + 2 + a - 7) * ((double) (a + b - 4 * a * b) / 2);
+    }
     public static void main(String[] args) {
         int a, b;
         a = 2;
         b = 3;
-        double formula = (9*a*a-5*b+2+a-7)*((double)(a+b-4*a*b)/2);
-        System.out.println(formula);
+        System.out.println(formula(a, b));
     }
 }
